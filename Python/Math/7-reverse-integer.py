bb66864ba3a7b07038608902f7f52762cbd01c40
Yena Lee <33945159+lee00286@users.noreply.github.com>
2020-06-19 00:55:49 +0900

    Create 7-reverse-integer.py

---
+++ Python/Math/7-reverse-integer.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        
+        >>> reverse(123)
+        321
+        >>> reverse(-123)
+        -321
+        >>> reverse(120)
+        21
+        """
+        
+        reverse_x = 0
+        neg = 1
+        zero_middle = 0
+        
+        # Negative case
+        if (x < 0):
+            neg = -1
+            x = -x
+
+        while (x != 0):
+            mod = x % 10
+            if (zero_middle != 0):
+                reverse_x = reverse_x * 100 + mod
+                zero_middle = 0
+            elif (mod != 0):
+                reverse_x = reverse_x * 10 + mod
+            else:
+                zero_middle += 1
+            x = x // 10
+        
+        if (reverse_x > 2147483649) or (reverse_x < -2147483649):
+            return 0
+        return neg * reverse_x
+        
