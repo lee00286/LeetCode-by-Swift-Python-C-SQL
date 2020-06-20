9bacad9de8b219db2f3117f0aeb86cbcfb709495
Yena Lee <33945159+lee00286@users.noreply.github.com>
2020-06-20 23:55:56 +0900

    Update 237_delete_node.c

--- C/Linked-List/237_delete_node.c
+++ C/Linked-List/237_delete_node.c
@@ -12,38 +12,16 @@ void deleteNode(struct ListNode* node) {
     // Traversal Pointer
     struct ListNode *tr = NULL;
     
+    // Copy val in node -> next to node
+    node -> val = node -> next -> val;
+    
+    // Set up pre and tr 
     pre = node;
     tr = node -> next;
     
-    // Copy the 
-    node -> val = tr -> val;
+    // Update pre
+    pre -> next = tr -> next;
     
-    // Predecessor Pointer
-    struct ListNode *pre = NULL;
-    // Traversal Pointer
-    struct ListNode *tr = NULL;
-    
-    // Set up pre and tr
-    pre = head;
-    tr = head -> next;
-    
-    // If node == head
-    if (pre -> val == node -> val) {
-        free(pre);
-        head = tr;
-    }
-    
-    // If node != head and node != tail
-    while (tr != NULL) {
-        // Found the node we want to delete
-        if (node -> val == tr -> val) {
-            // Update pre
-            pre -> next = tr -> next;
-            // Delete node
-            free(tr);
-            break;
-        }
-    }
-    
-    // Head didn't change
+    // Delete node
+    free(tr);
 }
