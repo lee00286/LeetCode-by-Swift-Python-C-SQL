ed96e06272806c72fce813de9106da17abd5d33f
Yena Lee <33945159+lee00286@users.noreply.github.com>
2020-06-23 22:34:25 +0900

    Create 263_ugly_number.py

---
+++ Python/Math/263_ugly_number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        
+        // 2 * 3 * 5 = 30
+        return num > 0 == 30 ** 32 % num
