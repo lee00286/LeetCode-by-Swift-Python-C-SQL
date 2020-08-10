9ae88679fd6af2244f9d1a939eae43dda55d21d1
Yena Lee <33945159+lee00286@users.noreply.github.com>
2020-08-10 21:59:51 +0900

    Update 0003_longest_substring_without_repeating_characters.py

--- Python/String/0003_longest_substring_without_repeating_characters.py
+++ Python/String/0003_longest_substring_without_repeating_characters.py
@@ -6,15 +6,15 @@ class Solution:
         if (len(s) == 1):
             return 1
         
-        count = 0
-        largest = 0
+        count = 1
+        largest = 1
         before = 0
-        for i in range(len(s)):
+        for i in range(1, len(s)):
             if (s[i] in s[before:i]):
                 if (count > largest):
                     largest = count
                 count = 0
-                before = i
+                before = i - 1
             else:
                 count += 1
         
