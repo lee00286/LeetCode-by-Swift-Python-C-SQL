843ebcf389bc3f2691fbf2fbc9964f50350d6edb
Yena Lee <33945159+lee00286@users.noreply.github.com>
2020-08-11 11:03:43 +0900

    Update 0003_longest_substring_without_repeating_characters.py

--- Python/String/0003_longest_substring_without_repeating_characters.py
+++ Python/String/0003_longest_substring_without_repeating_characters.py
@@ -6,16 +6,42 @@ class Solution:
         if (len(s) == 1):
             return 1
         
+        dicSub = {}
+        left = 0
+        count = 0
+        largest = 0
+        
+        for i in range(len(s)):
+            if (s[i] in dicSub):
+                count = i - left
+                largest = max(largest, count)
+                left = dicSub[s[i]] + 1
+            dicSub[s[i]] = i
+        
+        count = len(s[left:i])
+        return max(largest, count)
+    
+        '''
+        # Using for loop
+        
+        # Base Cases
+        if (s == ''):
+            return 0
+        if (len(s) == 1):
+            return 1
+        
         count = 1
         largest = 1
         before = 0
         for i in range(1, len(s)):
             if (s[i] in s[before:i]):
-                if (count > largest):
-                    largest = count
-                count = 0
+                largest = max(largest, count)
+                count = 1
                 before = i - 1
             else:
-                count += 1
+                count = i - before + 1
+        
+        largest = max(largest, count)
         
         return largest
+        '''
