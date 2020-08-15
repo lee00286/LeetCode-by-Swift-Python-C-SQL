f590a75eedbcebd1004e5f8a6f70f2ddb51dd401
Yena Lee <33945159+lee00286@users.noreply.github.com>
2020-08-15 23:57:40 +0900

    Update 0007_reverse_integer.swift

--- Swift/Math/0007_reverse_integer.swift
+++ Swift/Math/0007_reverse_integer.swift
@@ -1,27 +1,34 @@
 class Solution {
     func reverse(_ x: Int) -> Int {
-        reverse_x = 0
-        neg = 1
-        zero_middle = 0
+        var reverse_x = 0
+        var neg = 1
+        var zero_middle = 0
         
-        # Negative case
-        if (x < 0):
+        // Negative case
+        if (x < 0) {
             neg = -1
-            x = -x
+        }
+        var y = abs(x)
 
-        while (x != 0):
-            mod = x % 10
-            if (zero_middle != 0):
+        while (y != 0) {
+            var mod = y % 10
+            if (zero_middle != 0) {
                 reverse_x = reverse_x * 100 + mod
                 zero_middle = 0
-            elif (mod != 0):
+            }
+            else if (mod != 0) {
                 reverse_x = reverse_x * 10 + mod
-            else:
+            }
+            else {
                 zero_middle += 1
-            x = x // 10
+            }
+            y = y / 10
+        }
         
-        if (reverse_x > 2147483649) or (reverse_x < -2147483649):
+        if ((reverse_x > 2147483649) || (reverse_x < -2147483649)) {
             return 0
+        }
+        
         return neg * reverse_x
     }
 }
