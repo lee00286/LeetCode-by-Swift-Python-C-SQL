d8d551ee5ba16b5e4f0c03951dc9f592d25557fa
Yena Lee <33945159+lee00286@users.noreply.github.com>
2020-08-17 22:02:51 +0900

    Create 1351_count_negative_numbers_in_a_sorted_matrix.swift

---
+++ Swift/Math/1351_count_negative_numbers_in_a_sorted_matrix.swift
@@ -0,0 +1,39 @@
+class Solution {
+    func countNegatives(_ grid: [[Int]]) -> Int {
+        // Use the Idea of Binary Sort
+        // Function to find the first occurence of negative value
+        func findNegativeIndex(n: [[Int]]) -> Int {
+            if (n[0] < 0) {
+                return 0
+            }
+            
+            var left = 0
+            var right = n.count / 2
+            
+            while (right < n.count) {
+                // Negative value is in left side
+                if (n[right - 1] < 0) {
+                    right -= (right - left) / 2
+                }
+                // Negative value is n[right]
+                else if (n[right] < 0) {
+                    return right
+                }
+                // Negative value is in right side
+                else {
+                    left = right
+                    right += (len(n) - right) / 2
+                }
+            }
+        }
+        
+        var count = 0
+        for i in (0..<grid.count) {
+            // If there's negative value in grid[i]
+            if (min(grid[i]) < 0) {
+                count += grid[i].count - findNegativeIndex(grid[i])
+            }
+        }
+        return count
+    }
+}
