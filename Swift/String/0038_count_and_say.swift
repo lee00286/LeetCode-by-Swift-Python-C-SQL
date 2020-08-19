69a5561ebb140e7c43d53332bec8ea0de3623f05
Yena Lee <33945159+lee00286@users.noreply.github.com>
2020-08-19 21:49:09 +0900

    Create 0038_count_and_say.swift

---
+++ Swift/String/0038_count_and_say.swift
@@ -0,0 +1,44 @@
+class Solution {
+    func countAndSay(_ n: Int) -> String {
+        // Base Case: 1
+        // 2 = "11", 3 = "21", 4 = "1211", 5 = "111221"
+        
+        func countCon(num: str) -> str {
+            i = 0
+            cons = ""
+            latest = ""
+            count = 0
+            
+            // Count number of consecutives
+            for char in num:
+                // First consecutive
+                if (latest == ""):
+                    latest = char
+                    count += 1
+                else:
+                    // If consecutive
+                    if (latest == char):
+                        count += 1
+                    // If not consecutive
+                    else:
+                        cons += str(count) + latest
+                        latest = char
+                        count = 1
+            
+            cons += str(count) + latest
+            
+            return cons
+        }
+                    
+        // None Case
+        if (not n or n <= 0):
+            return ''
+        // Base Case
+        if (n == 1):
+            return '1'
+        // Recursive Case
+        else:
+            // Count consecutives
+            return countCon(self.countAndSay(n - 1))
+    }
+}
