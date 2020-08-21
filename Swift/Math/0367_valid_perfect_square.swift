b71a387f46fcb157c33ec4669e826ca24b3d33e0
Yena Lee <33945159+lee00286@users.noreply.github.com>
2020-08-21 21:58:55 +0900

    Create 0367_valid_perfect_square.swift

---
+++ Swift/Math/0367_valid_perfect_square.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func isPerfectSquare(_ num: Int) -> Bool {
+        var left = 0
+        var right = num
+        
+        while (left <= right) {
+            var mid: Int = left + (right - left) / 2
+            if (pow(mid, 2) == num) {
+                return true
+            }
+            else if (pow(mid, 2) > num) {
+                right = mid - 1
+            }
+            else {
+                left = mid + 1
+            }
+        }
+        return false
+    }
+}
