47ab16f5f8f05046895858cf89bab407b0470a5a
Yena Lee <33945159+lee00286@users.noreply.github.com>
2020-08-25 21:02:39 +0900

    Create 0005_longest_palindromic_substring.py

---
+++ Python/String/0005_longest_palindromic_substring.py
@@ -0,0 +1,28 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def isPalindrome(x):
+            length = len(x)
+            for i in range(int(length/2)):
+                if (x[i] != x[length - i - 1]): 
+                    return False
+            return True
+        
+        # If s is palindrome
+        if isPalindrome(s): 
+            return s
+        
+        dicPalin = {}
+        
+        length = len(s)
+        
+        for i in range(len(s) - 1):
+            j = 0
+            while (j < length):
+                if (isPalindrome(s[i:length - j])):
+                    dicPalin[i] = length - j
+                    break
+                j += 1
+        
+        key = max(dicPalin, key=dicPalin.get)
+    
+        return s[key:dicPalin[key]]
