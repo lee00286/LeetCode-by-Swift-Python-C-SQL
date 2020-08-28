4c88f82c586e50603734c73bfa7b66fb385784a8
Yena Lee <33945159+lee00286@users.noreply.github.com>
2020-08-28 22:03:06 +0900

    Update 0005_longest_palindromic_substring.py

--- Python/String/0005_longest_palindromic_substring.py
+++ Python/String/0005_longest_palindromic_substring.py
@@ -1,29 +1,23 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def isPalindrome(x):
-            length = len(x)
-            for i in range(int(length/2)):
-                if (x[i] != x[length - i - 1]): 
-                    return False
-            return True
-        
-        # If s is palindrome
-        if isPalindrome(s): 
-            return s
-        
-        dicPalin = {}
-        
-        length = len(s)
-        
-        for i in range(len(s) - 1):
-            j = 0
-            while (j < length):
-                if (s[length - j - 1] == s[i]):
-                    if (isPalindrome(s[i:length - j])):
-                        dicPalin[i] = length - j - i
-                        break
-                j += 1
-        
-        key = max(dicPalin, key=dicPalin.get)
+        def findPalin(l: int, r: int) -> str:
+            while (l >= 0 and r < len(s) and s[l] == s[r]):
+                l -= 1
+                r += 1
+            
+            return s[l + 1:r]
     
-        return s[key:dicPalin[key] + key]
+        palindrome = ""
+        
+        for i in range(len(s)):
+            # Find palin from left
+            substring = findPalin(i, i)
+            if (len(substring) > len(palindrome)):
+                palindrome = substring
+            
+            # Find palin from right
+            substring = findPalin(i, i + 1)
+            if (len(substring) > len(palindrome)):
+                palindrome = substring
+    
+        return palindrome
