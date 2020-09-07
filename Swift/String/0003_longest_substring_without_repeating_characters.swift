11533cb10dc3efe2c5ce6ae19a50adf14446824e
Yena Lee <33945159+lee00286@users.noreply.github.com>
2020-09-07 21:06:52 +0900

    Update 0003_longest_substring_without_repeating_characters.swift

--- Swift/String/0003_longest_substring_without_repeating_characters.swift
+++ Swift/String/0003_longest_substring_without_repeating_characters.swift
@@ -1,51 +1,26 @@
 class Solution {
-    func countAndSay(_ n: Int) -> String {
-        // Base Case: 1
-        // 2 = "11", 3 = "21", 4 = "1211", 5 = "111221"
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        // Base Cases
+        if (s == "") {
+            return 0
+        }
+        if (s.count == 1) {
+            return 1
+        }
         
-        func countCon(num: String) -> String {
-            var cons = ""
-            var latest = String()
-            var count = 0
-            
-            // Count number of consecutives
-            for char in num {
-                // First consecutive
-                if (latest.isEmpty) {
-                    latest = String(char)
-                    count += 1
-                }
-                else {
-                    // If consecutive
-                    if (latest == String(char)) {
-                        count += 1
-                    }
-                    // If not consecutive
-                    else {
-                        cons += String(count) + latest
-                        latest = String(char)
-                        count = 1
-                    }
-                }
+        //var dicSub = ["star": 0]
+        var dicSub: [Character: Int] = [:]
+        var left = 0
+        var largest = 0
+        
+        for (i, char) in s.enumerated() {
+            if (dicSub[char] != nil) {
+                largest = max(largest, i - left)
+                left = max(left, dicSub[char]! + 1)
             }
+            dicSub.updateValue(i, forKey: char)
+        }
             
-            cons += String(count) + latest
-            
-            return cons
-        }
-                    
-        // None Case
-        if (n <= 0) {
-            return ""
-        }
-        // Base Case
-        if (n == 1) {
-            return "1"
-        }
-        // Recursive Case
-        else {
-            // Count consecutives
-            return countCon(num: self.countAndSay(n - 1))
-        }
+        return max(largest, s.count - left)
     }
 }
