6f261f8aaafa3d53be60f43816f935d27adc9336
Yena Lee <33945159+lee00286@users.noreply.github.com>
2020-10-16 00:11:14 +0900

    Update 0029_divide_two_integers.py

--- Python/Math/0029_divide_two_integers.py
+++ Python/Math/0029_divide_two_integers.py
@@ -1,3 +1,28 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
+        negative = 1
+        # Return value is negative
+        if ((dividend < 0) ^ (divisor < 0)):
+            negative = -1
         
+        # Make dividend and divisor positive
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+        
+        # Don't need to calculate
+        if (divisor == 1):
+            # Overflow
+            if (negative * dividend > 2**31 - 1):
+                return 2**31 - 1
+            return negative * dividend
+        
+        quotient = 0
+        # Subtract dividend by divisor
+        while (dividend >= divisor):
+            dividend -= divisor
+            quotient += 1
+        
+        # Overflow
+        if (negative * quotient > 2**31 - 1):
+            return 2**31 - 1
+        return negative * quotient
