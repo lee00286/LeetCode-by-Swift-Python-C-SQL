d2ca23c38282c5701e4faf15d5d1bdfeb305ff2f
Yena Lee <33945159+lee00286@users.noreply.github.com>
2020-10-21 23:35:34 +0900

    Create 0067_add_binary.py

---
+++ Python/String/0067_add_binary.py
@@ -0,0 +1,8 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        if (a == '0' or a == ''):
+            return b
+        if (b == '0' or b == ''):
+            return a
+        
+        
