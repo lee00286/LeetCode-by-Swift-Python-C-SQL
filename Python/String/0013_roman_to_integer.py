5bee54dd7e38692edc8d1ddd95938ba97351ba1b
Yena Lee <33945159+lee00286@users.noreply.github.com>
2020-12-08 19:48:36 +0900

    Update 0013_roman_to_integer.py

--- Python/String/0013_roman_to_integer.py
+++ Python/String/0013_roman_to_integer.py
@@ -1,3 +1,18 @@
+# Dictionary Approach
 class Solution:
     def romanToInt(self, s: str) -> int:
+        romanDict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         
+        # Initialize
+        num = 0
+        prev = 0
+        
+        for char in s:
+            curr = romanDict[char]
+            if (curr > prev):
+                num = num + curr - 2 * prev
+            else:
+                num += curr
+            prev = curr
+        
+        return num
